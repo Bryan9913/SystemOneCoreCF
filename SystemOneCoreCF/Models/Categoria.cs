84039f5cfeb70b24ed57111724e0d643b3aeedce
Bryan Ernesto Martínez Pleitez <bryan.martinezpleitez99@outlook.com>
2020-09-09 18:33:47 -0600

    Bryan Martínez 09-09-2020
    Aplicación de Data Annotations a las entidades.

--- SystemOneCoreCF/Models/Categoria.cs
+++ SystemOneCoreCF/Models/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,12 @@ namespace SystemOneCoreCF.Models
     public class Categoria
     {
         public int Idcategoria { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage ="El nombre que ha ingresado no debe ser mayor a 50 ni menor a 3 caracteres")]
         public string Nombre { get; set; }
+
+        [StringLength(255, ErrorMessage = "El nombre que ha ingresado no debe ser mayor a 255 caracteres")]
         public string Descripcion { get; set; }
         public bool? Estado { get; set; }
 
--- SystemOneCoreCF/Models/Producto.cs
+++ SystemOneCoreCF/Models/Producto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,13 @@ namespace SystemOneCoreCF.Models
     public class Producto
     {
         public int Idproducto { get; set; }
+       
+        [Required (ErrorMessage = "Seleccione una categoría")]
         public int Idcategoria { get; set; }
         public string Codigo { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "El nombre que ha ingresado no debe ser mayor a 100 caracteres ni menos a 1")]
         public string Nombre { get; set; }
         public decimal PrecioVenta { get; set; }
         public int Stock { get; set; }
